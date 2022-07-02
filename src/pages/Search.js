d88f3dbda54b101453b61b48b4338e61a9421c9b
Carla Uyemura <carla.uyemura@gmail.com>
2022-07-02 13:16:17 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,13 +2,51 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonDisabled: true,
+    };
+  }
+
+  verificaInput = ({ target }) => {
+    const numMin = 2;
+    if (target.value.length >= numMin) {
+      this.setState({
+        buttonDisabled: false,
+      });
+    } else {
+      this.setState({ buttonDisabled: true });
+    }
+  };
+
   render() {
+    const { buttonDisabled } = this.state;
     return (
       <div
         data-testid="page-search"
       >
         <Header />
-        Search
+        <form>
+          <label htmlFor="input-search">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              id="input-search"
+              placeholder="Nome do Artista"
+              onChange={ this.verificaInput }
+            />
+          </label>
+
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ buttonDisabled }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
